9053b108db0268d1ea802b169e2438ac8c3c4952
m4gomedov <m4gomedov@internet.ru>
2024-11-27 11:53:19 +0300

    replace unittests with pytests

--- tests/test_task_1.py
+++ tests/test_task_1.py
@@ -1,45 +1,34 @@
-import unittest
+import sys
 from io import StringIO
-from unittest.mock import patch
+
+import pytest
 
 from src.task_1 import main
 
 
-class TestMainFunction(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        print(f"{cls.__name__:=^80}")
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        print("=" * 80)
-
-    @patch("builtins.input", side_effect=["5", "3"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_numeric_input(self, mock_stdout, mock_input):
-        main()
-        self.assertIn("Результат: 8.0", mock_stdout.getvalue())
-
-    @patch("builtins.input", side_effect=["abc", "xyz"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_string_input(self, mock_stdout, mock_input):
-        main()
-        self.assertIn("Результат: abcxyz", mock_stdout.getvalue())
-
-    @patch("builtins.input", side_effect=["10.5", "2.5"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_float_input(self, mock_stdout, mock_input):
-        main()
-        self.assertIn("Результат: 13.0", mock_stdout.getvalue())
-
-    @patch("builtins.input", side_effect=["123", "abc"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_mixed_input(self, mock_stdout, mock_input):
-        main()
-        self.assertIn("Результат: 123abc", mock_stdout.getvalue())
-
-    @patch("builtins.input", side_effect=["", ""])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_empty_input(self, mock_stdout, mock_input):
-        main()
-        self.assertIn("Результат: ", mock_stdout.getvalue())  # Ожидается пустая строка
+class TestMainFunction:
+    @pytest.mark.parametrize(
+        "user_input, expected_output",
+        [
+            ("5\n3\n", "Результат: 8.0"),
+            ("abc\nxyz\n", "Результат: abcxyz"),
+            ("10.5\n2.5\n", "Результат: 13.0"),
+            ("123\nabc\n", "Результат: 123abc"),
+            ("\n\n", "Результат: "),
+        ],
+    )
+    def test_main_function_no_mock(self, user_input, expected_output):
+        # Подмена стандартного ввода и вывода
+        original_stdin = sys.stdin
+        original_stdout = sys.stdout
+        try:
+            sys.stdin = StringIO(user_input)
+            output = StringIO()
+            sys.stdout = output
+            main()
+            result = output.getvalue()
+            assert expected_output in result
+        finally:
+            # Восстановление стандартного ввода и вывода
+            sys.stdin = original_stdin
+            sys.stdout = original_stdout
--- tests/test_task_2.py
+++ tests/test_task_2.py
@@ -1,59 +1,53 @@
-import unittest
+import sys
 from io import StringIO
-from unittest.mock import patch
+
+import pytest
 
 from src.task_2 import generate_matrix, main
 
 
-class TestMatrixGeneration(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        print(f"{cls.__name__:=^80}")
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        print("=" * 80)
-
-    def test_generate_matrix(self):
+class TestMainFunction:
+    def test_generate_matrix(
+        self,
+    ):
         # Проверяем корректную генерацию матрицы
         rows, cols, min_val, max_val = 3, 4, 1, 10
         matrix = generate_matrix(rows, cols, min_val, max_val)
-        self.assertEqual(len(matrix), rows)  # Проверяем количество строк
-        self.assertTrue(
-            all(len(row) == cols for row in matrix)
-        )  # Проверяем количество столбцов
-        self.assertTrue(
-            all(min_val <= value <= max_val for row in matrix for value in row)
-        )  # Проверяем диапазон значений
-
-    @patch("builtins.input", side_effect=["3", "4", "1", "10"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_main_valid_input(self, mock_stdout, mock_input):
-        main()
-        output = mock_stdout.getvalue()
-        self.assertIn("Сгенерированная матрица:", output)
-
-    @patch("builtins.input", side_effect=["0", "4", "1", "10"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_main_invalid_rows(self, mock_stdout, mock_input):
-        main()
-        output = mock_stdout.getvalue()
-        self.assertIn("Ошибка ввода", output)
-        self.assertIn("Число строк и столбцов должно быть больше нуля.", output)
-
-    @patch("builtins.input", side_effect=["3", "4", "10", "1"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_main_min_greater_than_max(self, mock_stdout, mock_input):
-        main()
-        output = mock_stdout.getvalue()
-        self.assertIn("Ошибка ввода", output)
-        self.assertIn(
-            "Минимальное значение не может быть больше максимального.", output
+        # Проверяем количество строк
+        assert len(matrix) == rows
+        # Проверяем количество столбцов
+        assert all(len(row) == cols for row in matrix)
+        # Диапазон
+        assert all(
+            min_val <= value <= max_val for row in matrix for value in row
         )
 
-    @patch("builtins.input", side_effect=["a", "4", "1", "10"])
-    @patch("sys.stdout", new_callable=StringIO)
-    def test_main_non_integer_input(self, mock_stdout, mock_input):
-        main()
-        output = mock_stdout.getvalue()
-        self.assertIn("Ошибка ввода", output)
+    @pytest.mark.parametrize(
+        "user_input, expected_output",
+        [
+            ("3\n4\n1\n10\n", "Сгенерированная матрица:"),
+            (
+                "0\n4\n1\n10\n",
+                "Число строк и столбцов должно быть больше нуля.",
+            ),
+            (
+                "3\n4\n10\n1\n",
+                "Минимальное значение не может быть больше максимального.",
+            ),
+            ("a\n4\n1\n10\n", "Ошибка ввода"),
+        ],
+    )
+    def test_main(self, user_input, expected_output):
+        # Подмена ввода и вывода
+        original_stdin = sys.stdin
+        original_stdout = sys.stdout
+        try:
+            sys.stdin = StringIO(user_input)
+            output = StringIO()
+            sys.stdout = output
+            main()
+            result = output.getvalue()
+            assert expected_output in result
+        finally:
+            sys.stdin = original_stdin
+            sys.stdout = original_stdout
